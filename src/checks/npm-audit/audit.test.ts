00ee93f3b6060c96ec5a23fcfdb075d8f6ad537c
Troels Liebe Bentsen <tlb@connectedcars.io>
2021-02-16 12:05:19 +0100

    Audit npm 7.x support

--- src/checks/npm-audit/audit.test.ts
+++ src/checks/npm-audit/audit.test.ts
@@ -1,5 +1,10 @@
 import { auditCheck } from './audit'
-import { auditMultiVulnerabilities, auditNoVulnerabilities } from './resources/audit-help-text'
+import {
+  auditMultiVulnerabilities,
+  auditMultiVulnerabilitiesV2,
+  auditNoVulnerabilities,
+  auditNoVulnerabilitiesV2
+} from './resources/audit-help-text'
 
 describe('checks/audit', () => {
   it('converts successful audit', () => {
@@ -13,11 +18,31 @@ describe('checks/audit', () => {
     })
   })
 
+  it('converts successful audit v2', () => {
+    const result = auditCheck({
+      data: auditNoVulnerabilitiesV2 as any,
+      sha: 'c61a4ae014360e064eb2a9f76c8a6a55d05e5b88'
+    })
+    expect(result).toMatchSnapshot({
+      completed_at: expect.stringMatching(/^\d{4}/)
+    })
+  })
+
   it('converts failed audit', () => {
     const data = JSON.parse(auditMultiVulnerabilities)
     const result = auditCheck({
       sha: 'abcdefg',
       data
+    })
+    expect(result).toMatchSnapshot({
+      completed_at: expect.stringMatching(/^\d{4}/)
+    })
+  })
+
+  it('converts failed audit v2', () => {
+    const result = auditCheck({
+      data: auditMultiVulnerabilitiesV2 as any,
+      sha: 'c61a4ae014360e064eb2a9f76c8a6a55d05e5b88'
     })
     expect(result).toMatchSnapshot({
       completed_at: expect.stringMatching(/^\d{4}/)
