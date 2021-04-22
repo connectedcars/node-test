17534ed337e9607c14a952338e688d99940c4eb3
vallentin <mail@vallentin.dev>
2021-04-22 16:02:43 +0200

    Added workaround cache issue

--- src/checks/cargo/run-cargo-check.ts
+++ src/checks/cargo/run-cargo-check.ts
@@ -1,7 +1,12 @@
 import { runJsonLinesCommand } from '../checks-common'
+import { touchRustFiles } from './cargo'
 import { CargoMessage } from './cargo-types'
 
 export async function runCargoCheck(args: string[] = []): Promise<CargoMessage[]> {
+  // Description for why this is needed, can be found
+  // at the `touchRustFiles` definition
+  await touchRustFiles()
+
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [exitInfo, json] = await runJsonLinesCommand<CargoMessage>('cargo', [
     'check',
--- src/checks/cargo/run-cargo-clippy.ts
+++ src/checks/cargo/run-cargo-clippy.ts
@@ -1,8 +1,13 @@
 import { runJsonLinesCommand } from '../checks-common'
+import { touchRustFiles } from './cargo'
 import { CargoMessage } from './cargo-types'
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function runCargoClippy(args: string[] = []): Promise<CargoMessage[]> {
+  // Description for why this is needed, can be found
+  // at the `touchRustFiles` definition
+  await touchRustFiles()
+
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [exitInfo, json] = await runJsonLinesCommand<CargoMessage>('cargo', [
     'clippy',
