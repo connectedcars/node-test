a667f7a16c7bf94711fe6879309833d622bd31c2
vallentin <mail@vallentin.dev>
2021-04-26 12:08:49 +0200

    Implemented options for touchFiles

--- src/unix/fs.ts
+++ src/unix/fs.ts
@@ -10,12 +10,20 @@ const fsStatAsync = util.promisify(fs.stat)
 const fsOpenAsync = util.promisify(fs.open)
 const fsCloseAsync = util.promisify(fs.close)
 const fsMkdirAsync = util.promisify(fs.mkdir)
+const fsUtimes = util.promisify(fs.utimes)
+
+export interface TouchFilesOptions {
+  /// Tests `excludeDir` against the directory names, not traversing matches
+  excludeDir?: RegExp
+  /// Tests `includeFile` against file names, only touching matches
+  includeFile?: RegExp
+}
 
 /**
  * Touch all files recursively from a given path
  * @param rootPath
  */
-export async function touchFiles(rootPath: string): Promise<void> {
+export async function touchFiles(rootPath: string, options?: TouchFilesOptions): Promise<void> {
   const dirs: string[] = [rootPath]
   while (dirs.length > 0) {
     const dir = dirs.shift() as string
@@ -23,12 +31,23 @@ export async function touchFiles(rootPath: string): Promise<void> {
       const fullPath = path.resolve(dir, file)
       const stat = await fsStatAsync(fullPath)
       if (stat.isDirectory()) {
+        if (options?.excludeDir?.test(file)) {
+          continue
+        }
         dirs.push(fullPath)
       } else if (stat.isFile()) {
-        await fsCloseAsync(await fsOpenAsync(fullPath, constants.O_RDWR))
+        if (options?.includeFile === undefined || options?.includeFile?.test(file)) {
+          await fsCloseAsync(await fsOpenAsync(fullPath, constants.O_RDWR))
+          await touchExistingFile(fullPath)
+        }
       }
     }
   }
+}
+
+async function touchExistingFile(path: string): Promise<void> {
+  const time = new Date()
+  await fsUtimes(path, time, time)
 }
 
 export async function createTempDirectory(): Promise<string> {
