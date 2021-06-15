16dc794b80fbb8be433718967a62e23571016bd4
vallentin <mail@vallentin.dev>
2021-06-15 09:57:29 +0200

    Removed duplicate code and made runCargo generic

--- src/checks/cargo/run-cargo-fmt.ts
+++ src/checks/cargo/run-cargo-fmt.ts
@@ -1,26 +1,16 @@
 import { runJsonCommand } from '../checks-common'
-import { getEnvRustFlags, touchRustFiles } from './cargo'
 import { CargoFmtFile } from './cargo-types'
+import { runCargo } from './run-cargo'
 
 export async function runCargoFmt(args: string[] = [], ci = true): Promise<CargoFmtFile[]> {
-  // Description for why this is needed, can be found
-  // at the `touchRustFiles` definition
-  await touchRustFiles()
-
   // While cargo and clippy emits individual json objects, rustfmt does not
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [exitInfo, json] = await runJsonCommand<CargoFmtFile[]>(
-    'cargo',
+  return runCargo<CargoFmtFile>(
     ['+nightly', 'fmt', '--', '--emit=json', '--color', 'never', ...args],
-    {
-      env: {
-        ...process.env,
-        RUSTFLAGS: getEnvRustFlags(ci)
-      }
-    }
+    ci,
+    runJsonCommand
   )
   // TODO: If there is syntax errors then `cargo fmt` fails, which results in `json` being empty
   //       and in turn `cargoFmtCheck` passing. It might be useful to handle that case. However,
   //       it is less important, as `checks cargo-fmt` won't be used alone.
-  return json
 }
