81f74d248ea822f5cbb678e641ed2ca2cefa3f7c
vallentin <mail@vallentin.dev>
2021-10-20 13:11:41 +0200

    Fixed cargo doc test for bin crates

--- src/checks/cargo/run-cargo-test.ts
+++ src/checks/cargo/run-cargo-test.ts
@@ -1,3 +1,4 @@
+import { CommandJSONConversionError } from '../checks-common'
 import { CargoMessage } from './cargo-types'
 import { runCargo } from './run-cargo'
 
@@ -21,19 +22,30 @@ export async function runCargoTest(args: string[] = [], ci = true): Promise<Carg
 
 // Issue: https://github.com/rust-lang/cargo/issues/6669
 export async function runCargoDocTest(args: string[] = [], ci = true): Promise<CargoMessage[]> {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  return runCargo(
-    [
-      'test',
-      '--doc',
-      '--locked',
-      '--message-format=json',
-      '--',
-      '-Zunstable-options',
-      '--format=json',
-      // '--report-time',
-      ...args
-    ],
-    ci
-  )
+  try {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    return await runCargo(
+      [
+        'test',
+        '--doc',
+        '--locked',
+        '--message-format=json',
+        '--',
+        '-Zunstable-options',
+        '--format=json',
+        // '--report-time',
+        ...args
+      ],
+      ci
+    )
+  } catch (e) {
+    if (e instanceof CommandJSONConversionError) {
+      if (e.stderr.trimLeft().startsWith('error: no library targets found in package')) {
+        // Executing `cargo test --doc` for non-library crates is not supported
+        return []
+      }
+    }
+
+    throw e
+  }
 }
