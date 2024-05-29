d6a36e94ef4e5b10e3e79a6eecf57bc3c79aef92
aab <aab@connectedcars.dk>
2024-05-29 09:32:00 +0200

    Unique descriptions, add another test

--- src/mysql/parse.test.ts
+++ src/mysql/parse.test.ts
@@ -2,7 +2,7 @@ import { parseCreateTableStatements } from './parse'
 
 describe('parser', () => {
   describe('parseCreateTableStatements', () => {
-    it('parses create table statements', async () => {
+    it('parses create table statements with missing collations', async () => {
       const sql = `CREATE TABLE UserPushTokens1 (
     id INT PRIMARY KEY AUTO_INCREMENT,
     userId INT NOT NULL,
@@ -39,7 +39,7 @@ CREATE TABLE UserPushTokens3
       expect(hasCollationCount).toEqual(0)
     })
 
-    it('parses create table statements', async () => {
+    it('parses create table statements with missing character sets', async () => {
       const sql = `CREATE TABLE UserPushTokens1 (
     id INT PRIMARY KEY AUTO_INCREMENT,
     userId INT NOT NULL,
@@ -74,6 +74,18 @@ CREATE TABLE UserPushTokens3
       expect(createTableStatementCount).toEqual(3)
       expect(hasCharacterSetCount).toEqual(0)
       expect(hasCollationCount).toEqual(2)
+    })
+
+    it('parses create table statements with no create table statements', async () => {
+      const sql = `ALTER TABLE UserPushTokens1 CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+
+ALTER TABLE UserPushTokens1 CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;`
+
+      const [createTableStatementCount, hasCharacterSetCount, hasCollationCount] = parseCreateTableStatements(sql)
+
+      expect(createTableStatementCount).toEqual(0)
+      expect(hasCharacterSetCount).toEqual(0)
+      expect(hasCollationCount).toEqual(0)
     })
 
     it('parses empty sql', async () => {
