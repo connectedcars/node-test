c3f5a1cac08a470d215822f506bc8c89f4339921
MisanthropicBit <aab@connectedcars.dk>
2025-11-21 14:02:46 +0100

    node-test cleanup (#148)

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,4 @@
 module.exports = {
   ...require('./node_modules/@connectedcars/setup/jest.config.js'),
-  roots: ['<rootDir>/src', '<rootDir>/bin'],
-  globalSetup: './src/test-setup.ts',
-  globalTeardown: './src/test-teardown.ts'
+  roots: ['<rootDir>/src']
 }
